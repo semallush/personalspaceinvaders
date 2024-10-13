feee77e12771415e7fe38cd4555320776d23c260
semallush <semallush@gmail.com>
2024-10-13 02:14:09 +0200

    epic pig chase en world edit god have mercy please dont ruin everything

--- pig.gd
+++ pig.gd
@@ -2,14 +2,18 @@ extends CharacterBody2D
 
 var astar_grid = AStarGrid2D.new()
 
-@onready
-var roomNode = get_node("../room")
+#@onready
+#var roomNode = get_node("../room")
+#
+#@onready
+#var worldNode = get_parent()
+#
+##@onready
+##var playerNode = get_node("../lenin")
 
-@onready
-var worldNode = get_parent()
-
-@onready
-var playerNode = get_node("../lenin")
+var roomNode = null
+var worldNode = null
+var playerNode = null
 
 var isStepping = false
 
@@ -18,7 +22,7 @@ var speed = 1
 var stepSize = 16
 var currentSubStep = 0
 
-
+var pigs = []
 # 0:up, 1:right, 2:down, 3:left
 var stepDir = -1
 var stepStart = Vector2i(0,0)
@@ -29,48 +33,99 @@ var pigTile = null
 var path = []
 var pathIndex = 0
 
+var pigIndex = -1
+
 func _ready():
 	pass
 
 	
 func _process(delta):
+	if (playerNode != null && worldNode != null && roomNode != null):
+
+		perp = playerNode.playerTile
+		
+		pigTile = Vector2i(floor((position.x)/16), floor((position.y)/16))
+		updateStep()
+		
+		# if this affects perfomance it needs to be called AFTER a new room is loaded
+		var cells = roomNode.get_used_cells()
+		var room = worldNode.rooms[worldNode.room_index]
+		var walls = worldNode.walls
+		astar_grid.region = Rect2i(walls["left"], walls["up"]+3, room.width-2, room.height-3)
+		astar_grid.cell_size = Vector2(16, 16)
+		astar_grid.diagonal_mode = 1
+		astar_grid.update()
 	
-	perp = playerNode.playerTile
-	pigTile = Vector2i(floor((position.x)/16), floor((position.y)/16))
-	updateStep()
-	
-	# if this affects perfomance it needs to be called AFTER a new room is loaded
-	var cells = roomNode.get_used_cells()
-	var room = worldNode.rooms[worldNode.room_index]
-	var walls = worldNode.walls
-	astar_grid.region = Rect2i(walls["left"], walls["up"]+3, room.width-2, room.height-3)
-	astar_grid.cell_size = Vector2(16, 16)
-	astar_grid.diagonal_mode = 1
-	astar_grid.update()
-	
-	
-	
-	if !isStepping:
-		if path.size() > 0 && pathIndex < path.size()-1:
-			var nextTile = path[pathIndex+1]
-			var dir = 0
-			#print(nextTile)
-			#print(pigTile)
-			#print('----')
-			#print(nextTile.y)
-			if nextTile.y < pigTile.y:
-				dir = 0
-			elif nextTile.x > pigTile.x:
-				dir = 1
-			elif nextTile.y > pigTile.y:
-				dir = 2
-			elif nextTile.x < pigTile.x:
-				dir = 3
-			else:
-				dir = -1
-			if dir > -1:
-				startStep(dir)
-			
+		
+		
+		if !isStepping:
+			if path.size() > 0 && pathIndex < path.size()-1:
+				var tileTaken = false
+				var nextTile = path[pathIndex+1]
+				
+				for pig in pigs:
+					if (pig.pigIndex != pigIndex):
+						var otherTile = Vector2i(floor((pig.position.x)/16), floor((pig.position.y)/16))
+						var otherNext = null
+						if pig.pathIndex < pig.path.size()-1:
+							otherNext = pig.path[pig.pathIndex+1]
+						if nextTile == otherTile || nextTile == otherNext:
+							tileTaken = true
+							break
+				
+				var dir = 0
+
+				if nextTile.y < pigTile.y:
+					dir = 0
+				elif nextTile.x > pigTile.x:
+					dir = 1
+				elif nextTile.y > pigTile.y:
+					dir = 2
+				elif nextTile.x < pigTile.x:
+					dir = 3
+				else:
+					dir = -1
+					
+				if dir > -1 && !tileTaken:
+					startStep(dir)
+				else:
+					#cant do pathfinding, do random step.
+					var left = Vector2i(pigTile.x-1, pigTile.y)
+					var right = Vector2i(pigTile.x+1, pigTile.y)
+					var up = Vector2i(pigTile.x, pigTile.y-1)
+					var down = Vector2i(pigTile.x, pigTile.y+1)
+					
+					# 0:up, 1:right, 2:down, 3:left
+					var dirPossibilities = [true, true ,true, true]
+					
+					for pig in pigs:
+						if pig.pigIndex != pigIndex:
+							var otherTile = Vector2i(floor((pig.position.x)/16), floor((pig.position.y)/16))
+							var otherNext = null
+							if pig.pathIndex < pig.path.size()-1:
+								otherNext = pig.path[pig.pathIndex+1]
+							if left == otherNext || left == otherTile:
+								dirPossibilities[3] = false
+							if right == otherNext || right == otherTile:
+								dirPossibilities[1] = false
+							if up == otherNext || up == otherTile:
+								dirPossibilities[0] = false
+							if down == otherNext || down == otherTile:
+								dirPossibilities[2] = false
+					
+					var dirs = []
+					if dirPossibilities[0]: dirs.append(0)
+					if dirPossibilities[1]: dirs.append(1)
+					if dirPossibilities[2]: dirs.append(2)
+					if dirPossibilities[3]: dirs.append(3)
+					
+					if randf()<0.05:
+						var di = randi_range(0,dirs.size()-1)
+						#print(dirs)
+						if (dirs.size()>0):
+							startStep(dirs[di])
+					
+				
 			
 
 	
@@ -99,6 +154,21 @@ func updateStep() -> void:
 				
 				
 func updatePath() -> void:
+
 	path = astar_grid.get_id_path(pigTile, perp, true)
 	pathIndex = 0
 	#print(path)
+func receivePlayer(player: Node) -> void:
+	playerNode = player
+func receiveWorld(world: Node) -> void:
+	worldNode = world
+func receiveRoom(room: Node) -> void:
+	roomNode = room
+func updatePigs(p:Array) -> void:
+	
+	pigs = p
+	for pig in pigs:
+		var otherTile = Vector2i(floor((pig.position.x)/16), floor((pig.position.y)/16))
+			
+		astar_grid.set_point_solid(otherTile, false)
+	
