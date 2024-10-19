d666bbf4bd3cdb5ebbf0d2076e75974a5f62817d
davidtiemstra <dntiemstra@hotmail.com>
2024-10-20 00:31:23 +0200

    fixed room sizes broke pigs

--- scripts/complex.gd
+++ scripts/complex.gd
@@ -156,11 +156,15 @@ func _process(delta: float) -> void:
 			room_index = newroom
 			load_room(room_index, door_translate[doorkey])
 		if(newroom == null):
-			
+			var door = rooms[room_index].doors[doorkey]
 			rooms.push_back(room.new(
 				room_index, 
 				door_translate[doorkey], 
-				rooms[room_index].world_coord + door_coords[doorkey] - Vector2i(walls["left"],walls["up"]),
+				rooms[room_index].world_coord
+					+ Vector2i(
+					door.coord if (doorkey=="up"||doorkey=="down") else walls["right"] - walls["left"] - 1 if (doorkey=="right") else 0, 
+					door.coord if (doorkey=="left"||doorkey=="right") else walls["down"] - walls["up"] - 1 if (doorkey=="down") else 0
+					),
 				rooms[room_index].doors[doorkey].mapped,
 				rooms,
 				rooms.size(),
@@ -168,8 +172,10 @@ func _process(delta: float) -> void:
 				wall_ornaments,
 				floor_ornaments
 			))
+			print('old coord:', rooms[room_index].world_coord)
 			rooms[room_index].doors[doorkey].room_index = rooms.size()-1
 			room_index = rooms.size()-1
+			print('new coord:', rooms[room_index].world_coord)
 			
 			precinctNode.updateCrossStar(rooms)
 			load_room(room_index, door_translate[doorkey])
@@ -184,17 +190,19 @@ func load_room(index, side) -> void:
 	var loading_room = rooms[index]
 	var tilestring
 	walls = {
-		"left": 10 - loading_room.width/2,
-		"right": 9 + loading_room.width/2,
+		"left": 9 - loading_room.width/2,
+		"right": 0,
 		"up": 10 - loading_room.height/2,
-		"down": 9 + loading_room.height/2,
+		"down": 0,
 	}
+	walls["right"] = walls["left"]+loading_room.width+1;
+	walls["down"] = walls["up"]+loading_room.height+1;
 	
 	roomNode.clear()
 	var writtenTiles = []
 	door_coords = default_doors
 	if(loading_room.doors["left"].exists):
-		var coord = Vector2i(walls["left"], walls["up"] + loading_room.doors["left"].coord)
+		var coord = Vector2i(walls["left"], walls["up"] + loading_room.doors["left"].coord-1)
 		roomNode.set_cell( coord, 0, tiles["door_left"])
 		writtenTiles.push_back(coord)
 		coord = Vector2i(coord.x + 1, coord.y + 2)
@@ -202,7 +210,7 @@ func load_room(index, side) -> void:
 		door_coords["left"] = coord
 		writtenTiles.push_back(coord)
 	if(loading_room.doors["right"].exists):
-		var coord = Vector2i(walls["right"], walls["up"] + loading_room.doors["right"].coord)
+		var coord = Vector2i(walls["right"], walls["up"] + loading_room.doors["right"].coord-1)
 		roomNode.set_cell( coord, 0, tiles["door_right"])
 		writtenTiles.push_back(coord)
 		coord = Vector2i(coord.x - 1, coord.y + 2)
@@ -210,7 +218,7 @@ func load_room(index, side) -> void:
 		door_coords["right"] = coord
 		writtenTiles.push_back(coord)
 	if(loading_room.doors["up"].exists):
-		var coord = Vector2i(walls["left"] + loading_room.doors["up"].coord, walls["up"])
+		var coord = Vector2i(walls["left"] + loading_room.doors["up"].coord+1, walls["up"] - 2)
 		roomNode.set_cell( coord, 0, tiles["door_up_top"])
 		writtenTiles.push_back(coord)
 		coord = Vector2i(coord.x, coord.y + 1)
@@ -224,7 +232,7 @@ func load_room(index, side) -> void:
 		door_coords["up"] = coord
 		writtenTiles.push_back(coord)
 	if(loading_room.doors["down"].exists):
-		var coord = Vector2i( walls["left"] + loading_room.doors["down"].coord, walls["down"])
+		var coord = Vector2i( walls["left"] + loading_room.doors["down"].coord+1, walls["down"] - 1)
 		roomNode.set_cell( coord, 0, tiles["doormat"])
 		door_coords["down"] = coord
 		writtenTiles.push_back(coord)
@@ -240,48 +248,48 @@ func load_room(index, side) -> void:
 				continue
 			elif( x < walls["left"] || 
 				x > walls["right"] || 
-				y < walls["up"] || 
-				y > walls["down"] ):
+				y < walls["up"]-2 || 
+				y > walls["down"]-1 ):
 				tilestring = "nothing"
-			elif( ( y < walls["down"] - 1) && (x == walls["left"] || (x == walls["right"])) || 
-				y == walls["up"] ):
+			elif(  y < walls["down"]  && (x == walls["left"] || (x == walls["right"])) || 
+				y == walls["up"]-2 ):
 				tilestring = "wall_top"
 			elif( (x > walls["left"] &&
 				  x < walls["right"] &&
-				(y == walls["up"] + 1 || y == walls["up"] + 2)) ||
+				(y == walls["up"] || y == walls["up"] - 1)) ||
 				 ((x == walls["left"] ||
 				  x == walls["right"]) &&
-				(y == walls["down"] - 1 || y == walls["down"]))):
+				(y == walls["down"] + 1 || y == walls["down"]))):
 				tilestring = "wall_side"
 			else:
 				roomNode.set_cell(Vector2i(x, y), 0, loading_room.floor_tile)
 				continue
 			roomNode.set_cell(Vector2i(x, y), 0, tiles[tilestring])
 			
-	for ornament in rooms[room_index].this_floor_ornament:
-		var obs_sprite = Sprite2D.new()
-		var texture
-		for ornament_texture in floor_ornaments:
-			if(ornament_texture["name"] == ornament["name"]):
-				texture = ornament_texture["texture"]
-		obs_sprite.set_texture(texture)
-		obs_sprite.rotate(ornament["rot"]*PI/2)
-		obs_sprite.set_centered(false)
-		obs_sprite.translate(Vector2( (walls["left"]+ornament["pos"].x+2)*16, (walls["up"]+ornament["pos"].y+4)*16))
-		furniture_layer.add_child(obs_sprite)
-		obstacle_nodes.push_back(obs_sprite)
-	for obstacle in rooms[room_index].this_obstacles:
-		var obs_sprite = Sprite2D.new()
-		var texture
-		for obstacle_texture in obstacles:
-			if(obstacle_texture["name"] == obstacle["name"]):
-				texture = obstacle_texture["texture"]
-		obs_sprite.set_texture(texture)
-		obs_sprite.rotate(obstacle["rot"]*PI/2)
-		obs_sprite.set_centered(false)
-		obs_sprite.translate(Vector2( (walls["left"]+obstacle["pos"].x+2)*16, (walls["up"]+obstacle["pos"].y+4)*16))
-		furniture_layer.add_child(obs_sprite)
-		obstacle_nodes.push_back(obs_sprite)
+	#for ornament in rooms[room_index].this_floor_ornament:
+		#var obs_sprite = Sprite2D.new()
+		#var texture
+		#for ornament_texture in floor_ornaments:
+			#if(ornament_texture["name"] == ornament["name"]):
+				#texture = ornament_texture["texture"]
+		#obs_sprite.set_texture(texture)
+		#obs_sprite.rotate(ornament["rot"]*PI/2)
+		#obs_sprite.set_centered(false)
+		#obs_sprite.translate(Vector2( (walls["left"]+ornament["pos"].x+2)*16, (walls["up"]+ornament["pos"].y+4)*16))
+		#furniture_layer.add_child(obs_sprite)
+		#obstacle_nodes.push_back(obs_sprite)
+	#for obstacle in rooms[room_index].this_obstacles:
+		#var obs_sprite = Sprite2D.new()
+		#var texture
+		#for obstacle_texture in obstacles:
+			#if(obstacle_texture["name"] == obstacle["name"]):
+				#texture = obstacle_texture["texture"]
+		#obs_sprite.set_texture(texture)
+		#obs_sprite.rotate(obstacle["rot"]*PI/2)
+		#obs_sprite.set_centered(false)
+		#obs_sprite.translate(Vector2( (walls["left"]+obstacle["pos"].x+2)*16, (walls["up"]+obstacle["pos"].y+4)*16))
+		#furniture_layer.add_child(obs_sprite)
+		#obstacle_nodes.push_back(obs_sprite)
 		
 	precinctNode.killPigs()
 	
@@ -290,16 +298,16 @@ func load_room(index, side) -> void:
 func map_room() -> void:
 	rooms[room_index].mapped = true
 	
-	var roomsize = Vector2(rooms[room_index].width-2, rooms[room_index].height-2)
+	var roomsize = Vector2(rooms[room_index].width, rooms[room_index].height)
 	var roomcoord = rooms[room_index].world_coord
 	
 	var new_room_map_outside = ColorRect.new()
 	new_room_map_outside.name = str(room_index)
 	minimap.add_child(new_room_map_outside)
 	new_room_map_outside.set_begin(
-		minimap.get_size()/2 + Vector2(roomcoord)
+		minimap.get_size()/2 + Vector2(roomcoord) + Vector2(-1,-1) + Vector2(rooms[room_index].map_offset)
 	)
-	new_room_map_outside.set_size(roomsize)
+	new_room_map_outside.set_size(roomsize + Vector2(2,2))
 	new_room_map_outside.set_color(linecolor)
 	
 	var new_room_map_inside = ColorRect.new()
@@ -323,9 +331,8 @@ func map_room() -> void:
 		door_line.name = doorkey
 		new_room_map_outside.add_child(door_line)
 		door_line.set_begin(Vector2i(
-			door.coord if (doorkey=="up"||doorkey=="down") else roomsize.x if (doorkey=="right") else 0, 
-			door.coord if (doorkey=="left"||doorkey=="right") else roomsize.y if (doorkey=="down") else 0)
-			+ doormapoffset[doorkey]
+			door.coord if (doorkey=="up"||doorkey=="down") else roomsize.x + 1 if (doorkey=="right") else 0, 
+			door.coord if (doorkey=="left"||doorkey=="right") else roomsize.y  + 1 if (doorkey=="down") else 0)
 		)
 		door_line.set_size(doormaplinesize[doorkey])
 		door_line.set_color(doorcolor if rooms[room_index].doors[doorkey].mapped else linecolor)
--- scripts/room.gd
+++ scripts/room.gd
@@ -33,6 +33,8 @@ var doors  = {
 	}
 }
 
+var map_offset = Vector2i(0,0)
+
 var this_wall_ornament = []
 var this_floor_ornament = []
 var this_obstacles = []
@@ -50,40 +52,38 @@ var door_door_shift = {
 	"up"=	Vector2i(-1,0),
 	"down"=	Vector2i(-1,0)
 }
-var door_door_offset = { #this is necessary because of how badly i fucked up the room loading
-	"left"=	Vector2i(-1, -2),
-	"right"=Vector2i(0,-2),
-	"up"=	Vector2i(-1,-2),
-	"down"=	Vector2i(0,-3)
+var door_door_offset = { #this is only for the mapping now
+	"left"=	Vector2i(1,0),
+	"right"=Vector2i(-1,0),
+	"up"=	Vector2i(0,1),
+	"down"=	Vector2i(0,-1)
 }
 
 var floor_tile = Vector2i(randi_range(0,1),randi_range(0,1))
 
 func _init(start_index, start_side, door_coord, mapped, rooms, index, obstacles, wall_ornaments, floor_ornaments) -> void:
 	
+	print('door', door_coord)
+	
 	room_index = index
-	if(start_side):check_convergence(start_side, door_coord, rooms)
+	if(start_side):
+		check_convergence(start_side, door_coord, rooms)
 	if(width == 0):
 		width = randi_range(max(6,doors["up"].coord, doors["down"].coord),18)
 	width += width%2
 	width = max(width,6)
 	if(height == 0):
-		height = randi_range(max(6,doors["left"].coord, doors["right"].coord),18)
+		height = randi_range(max(6,doors["left"].coord, doors["right"].coord),16)
 	height += height%2
 	height = max(height,6)
 	
-	if(start_side):
-		world_coord = (door_coord 
-			+ door_size_shift[start_side] * Vector2i(width-2, height-3)
-			+ door_door_shift[start_side] * Vector2i(doors[start_side].coord,doors[start_side].coord)
-			+ door_door_offset[start_side]
-		)
+	print('size ', width, ", ", height)
 		
 	var check_rooms_direction = {
-		"left"=	Vector2i(-12, floor(height/2)),
-		"right"=Vector2i(width+12,floor(height/2)),
-		"up"=	Vector2i(floor(width/2),-12),
-		"down"=	Vector2i(floor(width/2), height+12)
+		"left"=	Vector2i(-1, floor(height/2)),
+		"right"=Vector2i(width+1,floor(height/2)),
+		"up"=	Vector2i(floor(width/2),-1),
+		"down"=	Vector2i(floor(width/2), height+1)
 	}
 		
 	for door in doors:
@@ -96,11 +96,11 @@ func _init(start_index, start_side, door_coord, mapped, rooms, index, obstacles,
 			doors[door].mapped = rooms[doors[door].room_index].doors[door_translate[door]].mapped
 		else:
 			#check if theres some shit in the way????
-			var samplepos = world_coord+check_rooms_direction[door]
-			for room in rooms:
-				if(samplepos.x > room.world_coord.x && samplepos.y > room.world_coord.y
-					&& samplepos.x < room.world_coord.x + room.width && samplepos.y < room.world_coord.y + room.height):
-						continue
+			#var samplepos = world_coord+check_rooms_direction[door]
+			#for room in rooms:
+				#if(samplepos.x > room.world_coord.x && samplepos.y > room.world_coord.y
+					#&& samplepos.x < room.world_coord.x + room.width && samplepos.y < room.world_coord.y + room.height):
+						#continue
 				
 			doors[door].exists = randf() < 0.75
 			
@@ -108,12 +108,22 @@ func _init(start_index, start_side, door_coord, mapped, rooms, index, obstacles,
 			if(door=="left" || door=="right" ): doors[door].coord = min(doors[door].coord, height-5)
 			continue
 		if(door == "left" || door == "right"):
-			doors[door].coord = randi_range(1,height-5)
+			doors[door].coord = randi_range(0,height-1)
 		if(door == "up" || door == "down"):
-			doors[door].coord = randi_range(1,width-3)
+			doors[door].coord = randi_range(0,width-1)
+	
+	if(start_side):
+		map_offset = rooms[start_index].map_offset + door_door_offset[start_side]
+		world_coord = (door_coord 
+			+ door_size_shift[start_side] * Vector2i(width, height)
+			+ door_door_shift[start_side] * Vector2i(doors[start_side].coord,doors[start_side].coord)
+		)
+		
+	if(start_side):
+		print('doorstart coord, ', doors[start_side].coord)
 	
 	# add obstacles
-	var roomsize = Vector2i(width-4, height-5)
+	var roomsize = Vector2i(width, height)
 	var total_tiles = roomsize.x * roomsize.y
 	while(obstacle_tiles.size() / total_tiles < 0.25):
 		if(randf()>0.999):
@@ -143,7 +153,7 @@ func _init(start_index, start_side, door_coord, mapped, rooms, index, obstacles,
 				"name" = obstacle.name,
 				"rot" = rotate
 			})
-	print(this_obstacles)
+	#print(this_obstacles)
 	
 	#add floor deco
 	var filled_tiles = [];
@@ -183,129 +193,7 @@ func verify_mapping() -> void:
 		mapped_correctly = true
 
 func check_convergence(start_side, door_coord, rooms) -> void:
-	var convergence_check_box = Vector2i(18,18)
-	var check_areas = {
-		"left_corner" = Vector2i(),
-		"right_corner" = Vector2i(),
-		"opposite" = Vector2i()
-	}
-	
-	var check_dir = check_direction[start_side]
-	var check_window = {
-		"x0"= door_coord.x + check_dir.x*18,
-		"y0"= door_coord.y + check_dir.y*18,
-		"x1"= door_coord.x + check_dir.z*18,
-		"y1"= door_coord.y + check_dir.w*18
-	}
-	
-	var convergent_doors = []
-	
-	for room in rooms:
-		var roomsize = Vector2i(room.width-2, room.height-2)
-		for doorkey in room.doors:
-			var door = room.doors[doorkey]
-			if(!door.exists || doorkey==door_translate[start_side] || door.room_index != null):
-				continue
-				
-			var doorcoord = room.world_coord + Vector2i(
-				door.coord if (doorkey=="up"||doorkey=="down") else roomsize.x if (doorkey=="right") else 0, 
-				door.coord if (doorkey=="left"||doorkey=="right") else roomsize.y if (doorkey=="down") else 0)
-			
-			if(doorcoord.x > check_window["x0"] && doorcoord.y > check_window["y0"] &&
-				doorcoord.x < check_window["x1"] && doorcoord.y < check_window["y1"]):
-					var betterwindow = door_options[start_side][door_translate[doorkey]] * 18
-					var bettercheck = {
-						"x0"= door_coord.x + betterwindow.x,
-						"y0"= door_coord.y + betterwindow.y,
-						"x1"= door_coord.x + betterwindow.z,
-						"y1"= door_coord.y + betterwindow.w
-					}
-					if(doorcoord.x > bettercheck["x0"] && doorcoord.y > bettercheck["y0"] &&
-						doorcoord.x < bettercheck["x1"] && doorcoord.y < bettercheck["y1"]):
-							if(start_side=="left"||start_side=="right"):
-								convergent_doors.push_back({
-									"key"=door_translate[doorkey],
-									"doorcoord"=doorcoord,
-									"door"=door,
-									"room_index"=room.room_index
-								})
-								doors[door_translate[doorkey]].exists = true
-								doors[door_translate[doorkey]].room_index = room.room_index
-								door.room_index = room_index
-	
-	if convergent_doors.size() > 0:
-		print('this room is: ', room_index)
-		print('convergent doors ', convergent_doors)
-		print('my doors are ', doors)
-		
-		var door = convergent_doors[0]
-		var ydiff = door["doorcoord"].y - door_coord.y
-		var xdiff = door["doorcoord"].x - door_coord.x
-		if start_side=="left":
-			match door["key"]:
-				"up":
-					doors["left"].coord = door_coord.y - door["doorcoord"].y
-					doors["up"].coord = door["doorcoord"].x - door_coord.x
-				"right":
-					if(ydiff > 0):
-						doors["left"].coord = randi_range(1,18-ydiff)
-					else:
-						doors["left"].coord = randi_range(1-ydiff,18)
-					doors["right"].coord = doors["left"].coord + ydiff
-					width = xdiff
-				"down":
-					doors["left"].coord = randi_range(1,18-ydiff)
-					height = doors["left"].coord + ydiff
-					doors["down"].coord = door["doorcoord"].x - door_coord.x
-		if start_side=="right":
-			match door["key"]:
-				"up":
-					doors["right"].coord = door_coord.y - door["doorcoord"].y
-					doors["up"].coord = randi_range(1,18+xdiff)
-					width = doors["up"].coord - xdiff
-				"left":
-					width = -1*xdiff
-					if(ydiff > 0):
-						doors["right"].coord = randi_range(1,18-ydiff)
-					else:
-						doors["right"].coord = randi_range(1-ydiff,18)
-					doors["left"].coord = doors["right"].coord + ydiff
-				"down":
-					doors["down"].coord = randi_range(1,18+xdiff)
-					width = doors["down"].coord - xdiff
-					
-					doors["right"].coord = randi_range(1,18-ydiff)
-					height = doors["right"].coord + ydiff
-		#if start_side=="up":
-			#match door["key"]:
-				#"up":
-					#doors["left"].coord = door_coord.y - door["doorcoord"].y
-					#doors["up"].coord = door["doorcoord"].x - door_coord.x
-				#"right":
-					#if(ydiff > 0):
-						#doors["left"].coord = randi_range(1,18-ydiff)
-					#else:
-						#doors["left"].coord = randi_range(1-ydiff,18)
-					#doors["right"].coord = doors["left"].coord + ydiff
-				#"down":
-					#doors["left"].coord = randi_range(1,18-ydiff)
-					#height = doors["left"] + ydiff
-		#if start_side=="down":
-			#match door["key"]:
-				#"up":
-					#doors["left"].coord = door_coord.y - door["doorcoord"].y
-					#doors["up"].coord = door["doorcoord"].x - door_coord.x
-				#"right":
-					#if(ydiff > 0):
-						#doors["left"].coord = randi_range(1,18-ydiff)
-					#else:
-						#doors["left"].coord = randi_range(1-ydiff,18)
-					#doors["right"].coord = doors["left"].coord + ydiff
-				#"down":
-					#doors["left"].coord = randi_range(1,18-ydiff)
-					#height = doors["left"] + ydiff
-						
-		
+	pass
 
 var door_options = {
 	"left"={
@@ -328,20 +216,6 @@ var door_options = {
 		"right" = Vector4i(0,-1,1,0),
 		"up" = 	  Vector4i(-1,-1,1,0), 
 	},
-}
-
-var door_coord_translate = {
-	"left"=	Vector2i(0,1), #check area relative to door: x1,y1,x2,y2
-	"right"=Vector2i(0,1),
-	"up"=	Vector2i(1,-1),
-	"down"=	Vector2i(1,0)
-}
-
-var check_direction = {
-	"left"=	Vector4i(0,-1,1,1), #check area relative to door: x1,y1,x2,y2
-	"right"=Vector4i(-1,-1,0,1),
-	"up"=	Vector4i(-1,0,1,-1),
-	"down"=	Vector4i(-1,-1,1,0)
 }
 
 var door_translate = {
